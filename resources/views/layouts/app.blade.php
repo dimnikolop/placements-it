554ac535ac4c9e2d1a061a01ed86a489c743c404
Dimitris Nikolopoulos <dnikolopoulos89@gmail.com>
2020-11-25 16:30:29 +0200

    Added Laravel REST API

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Placements</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}"></link>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</head>
+<body>
+    @yield('content')
+</body>
+</html>
